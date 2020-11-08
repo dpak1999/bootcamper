41aa32354aec1f3fd2fb847c4b6d1b321063d5dd
dpak1999 <deepakkumardash313@gmail.com>
2020-11-08 23:28:57 +0530

    selecting and sorting

---
+++ _data/test.js
@@ -0,0 +1,33 @@
+const ErrorResponse = require("../utils/errorResponse");
+const asyncHandler = require("../middleware/async");
+const geocoder = require("../utils/geocoder");
+const Bootcamp = require("../models/Bootcamp");
+
+// @desc      Get all bootcamps
+// @route     GET /api/v1/bootcamps
+// @access    Public
+exports.getBootcamps = asyncHandler(async (req, res, next) => {
+  let query;
+
+  const reqQuery = { ...req.query };
+
+  const removeFields = ["select", "sort", "page", "limit"];
+
+  removeFields.forEach((param) => delete reqQuery[param]);
+
+  let queryStr = JSON.stringify(reqQuery);
+
+  queryStr = queryStr.replace(
+    /\b(gt|gte|lt|lte|in)\b/g,
+    (match) => `$${match}`
+  );
+
+  query = Bootcamp.find(JSON.parse(queryStr));
+
+  if (req.query.select) {
+    const fields = req.query.select.split(",").join(" ");
+    query = query.select(fields);
+  }
+
+  const bootcamps = await query;
+});
