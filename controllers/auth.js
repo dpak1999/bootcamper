f52def5eab9bf864fbaf4901440a2145848a5912
dpak1999 <deepakkumardash313@gmail.com>
2020-11-15 19:59:51 +0530

    creating login route and controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,7 +4,7 @@ const Bootcamp = require("../models/User");
 const User = require("../models/User");
 
 // @desc    Register User
-// @route   GET /api/v1/auth/register
+// @route   POST /api/v1/auth/register
 // @access  Public
 exports.register = asyncHandler(async (req, res, next) => {
   const { name, email, password, role } = req.body;
@@ -16,6 +16,38 @@ exports.register = asyncHandler(async (req, res, next) => {
     password,
     role,
   });
+
+  //   create token
+  const token = user.getSignedJwtToken();
+
+  res.status(200).json({ success: true, token });
+});
+
+// @desc    Login User
+// @route   POST /api/v1/auth/login
+// @access  Public
+exports.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // validate email and password
+  if (!email || !password) {
+    return next(
+      new ErrorResponse("Please enter a valid email and password", 400)
+    );
+  }
+
+  //   check for user
+  const user = await User.findOne({ email }).select("+password");
+  if (!user) {
+    return next(new ErrorResponse("Invalid credentials", 401));
+  }
+
+  //   check if password matches
+  const isMatch = await user.matchPassword(password);
+
+  if (!isMatch) {
+    return next(new ErrorResponse("Invalid credentials", 401));
+  }
 
   //   create token
   const token = user.getSignedJwtToken();
